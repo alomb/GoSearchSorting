7523f0b228564ac282e33833ede3ca2dc1ca2e44
alessandro <alelombo55@gmail.com>
2019-12-27 20:11:53 +0100

    Quick sort implemented.

---
+++ sorting/quicksort/quicksort.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"gosearchsorting/sorting/test"
+	"gosearchsorting/utils"
+)
+
+// QuickSort in the classical recursive 2-way form
+func QuickSort(array []int, length int) []int {
+	quicksort(array, 0, length-1)
+	return array
+}
+
+// Returns the correct index of the last element of the subarray
+func getRightIndex(array []int, min int, max int) int {
+	index := min
+
+	// Find the index moving greater values to the right of the index and lower to the left
+	for j := min; j < max; j++ {
+		if array[j] < array[max] {
+			utils.Swap(array, j, index)
+			index++
+		}
+	}
+
+	// Finally put the value in the right position: this is the real ordering step
+	utils.Swap(array, index, max)
+	return index
+}
+
+// The algorithm core
+func quicksort(array []int, min int, max int) {
+	// If the array has length strictly greater than 1
+	if min < max {
+		// Compute the correct index
+		index := getRightIndex(array, min, max)
+
+		// From latin <3 "Divide et impera"
+		quicksort(array, min, index-1)
+		quicksort(array, index+1, max)
+	}
+}
+
+func main() {
+	fmt.Println("Quick Sort")
+	test.Test(QuickSort)
+}
